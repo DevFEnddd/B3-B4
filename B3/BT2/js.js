6a9c2a449a770326e9beecae94a94bd3f0f77a52
DevFEnddd <88378029+DevFEnddd@users.noreply.github.com>
2021-08-16 18:31:48 +0700

    Update js.js

--- B3/BT2/js.js
+++ B3/BT2/js.js
@@ -1,14 +1,13 @@
 // Write a script asking users to enter a sequence of numbers, 
 // the numbers are separated by commas, find the smallest number and log it out to users
-let n, sum,min;
-n = Number(prompt("Nhập số lượng phần tử bạn muốn nhập: "));
-let dayso = new Array(n);
-for (let i = 0; i < n; i++) {
-    dayso[i] = Number(prompt("Enter number: " + (i + 1)));
-     min = dayso[0];
-    if (dayso[i] < min) {
-        min = dayso[i];
-    }
-}
-alert(min);
+let arr = prompt("Enter the number sequence: ");
+let newArr = arr.split(',');
+let minValue = newArr[0];
 
+for (let i = 0; i < newArr.length; i++) {
+         
+      if (minValue > Number(newArr[i])) {
+               minValue = Number(newArr[i])
+      }
+}
+alert(minValue)
